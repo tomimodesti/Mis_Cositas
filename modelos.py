36c5d978fac2c0017358c092861baa728224bb7e
tomimodesti <165415794+tomimodesti@users.noreply.github.com>
2024-06-07 19:04:30 -0300

    Add files via upload

---
+++ modelos.py
@@ -0,0 +1,21 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    books = db.relationship("Book")
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    cant_pages = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
